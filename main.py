038e76af61dff9d1242c19e66a2fc2f1af83d816
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-02 18:53:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ if(st.button('Submit')):
              st.info("Song not found")
      except Exception:
          st.info("not found")
-         return
      try:
          with yt_dlp.YoutubeDL(ydl_opts) as ydl:
              info_dict = ydl.extract_info(link, download=False)
