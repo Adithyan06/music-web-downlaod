ceb8827c573e3ca09f284f536aa148530f1829d2
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-02 18:26:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,51 +13,47 @@ st.set_page_config(page_title="Download Now or later",page_icon="images/logo.png
     
 })
 
-async def download (query,res):
-    st.title("Download Youtube Video")
-    query=st.text_input("Youtube Video or Playlist URL")
-    ydl_opts = {
-            "format": "bestaudio",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": "mp3",
-                    "preferredquality": "320",
-                }
-            ],
-            "outtmpl": "%(alt_title)s.mp3",
-            "quiet": True,
-            "logtostderr": False,
-    }
-    try:
-        results = []
-        count = 0
-        while len(results) == 0 and count < 6:
-            if count>0:
-                time.sleep(1)
-            results = YoutubeSearch(query, max_results=1).to_dict()
-            count += 1
-        try:
-            link = f"https://youtube.com{results[0]['url_suffix']}"
-            title = results[0]["title"]
-        except Exception:
-            st.info("Song not found")
-            return
-    except Exception:
+st.title("Download Youtube Video")
+query = st.text_input("Youtube Video or Playlist URL")
+if(st.button('Submit')):
+     ydl_opts = {
+             "format": "bestaudio",
+             "addmetadata": True,
+             "key": "FFmpegMetadata",
+             "prefer_ffmpeg": True,
+             "geo_bypass": True,
+             "nocheckcertificate": True,
+             "postprocessors": [
+                 { 
+                     "key": "FFmpegExtractAudio",
+                     "preferredcodec": "mp3",
+                     "preferredquality": "320",
+                 }
+             ],
+                     "outtmpl": "%(alt_title)s.mp3",
+                     "quiet": True,
+                     "logtostderr": False,
+     }
+     try:
+         results = []
+         count = 0
+         while len(results) == 0 and count < 6:
+             if count>0:
+                 time.sleep(1)
+             results = YoutubeSearch(query, max_results=1).to_dict()
+             count += 1
+         try:
+             link = f"https://youtube.com{results[0]['url_suffix']}"
+             title = results[0]["title"]
+         except Exception:
+             st.info("Song not found")
+             return
+     except Exception:
         st.info("not found")
         return
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(link, download=False)
-                audio = ydl.prepare_filename(info_dict)
-                ydl.process_info(info_dict)
-                q=Path(audio)
-
-                a=st.button("Start Downloading 🙂")  
-                if a:
-                    st.audio(audio)
+     try:
+         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+             info_dict = ydl.extract_info(link, download=False)
+             audio = ydl.prepare_filename(info_dict)
+             ydl.process_info(info_dict)
+             st.audio(audio)
