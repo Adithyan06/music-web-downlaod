956a9fe87ca1ef10e56a4eb420573ed23bfb4bf3
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-02 18:40:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,5 +57,5 @@ if(st.button('Submit')):
              info_dict = ydl.extract_info(link, download=False)
              audio = ydl.prepare_filename(info_dict)
              ydl.process_info(info_dict)
-             w = wget.download("audio.mp3",audio)
-             st.audio(w)
+             w = wget.download(audio)
+             st.audio("audio.mp3",w)
