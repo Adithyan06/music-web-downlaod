c7e9431e8bbb4ed84878f694dfd577a236704f15
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-02 17:51:41 +0530

    Update image.py

--- image.py
+++ image.py
@@ -5,15 +5,15 @@ import streamlit as st
 import requests
 from pathlib import Path
 
-st.set_page_config(page_title="Download Now",page_icon="images/logo.png",menu_items={
+st.set_page_config(page_title="Download Now",page_icon="kannan/logo.png",menu_items={
     "Get help": "https://github.com/dudegladiator/YoutubeDownloader",
     "Report a bug" : "https://github.com/dudegladiator/YoutubeDownloader/issues"
     
 })                       
 
 st.cache()
-st.title("Download Youtube Video")
-query = st.text_input("Youtube Video or Playlist URL")
+st.title("Download Cover Images of any Song")
+query = st.text_input("Enter a Movie Name")
 if(st.button('Submit')):
      result = query.title()
      st.success(result)
@@ -24,3 +24,5 @@ if(st.button('Submit')):
      thums = urlhp["album"]["cover_big"]
      thumb = wget.download(thums)
      st.image(thumb)
+     st.download_button("Save Image",thumb")           
+                    
