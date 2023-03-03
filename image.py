1fd892600681af78d428f42355455435f8e52edd
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-03 13:50:31 +0530

    Update image.py

--- image.py
+++ image.py
@@ -5,6 +5,20 @@ import streamlit as st
 import requests
 from pathlib import Path
 
+
+API = "https://apis.xditya.me/lyrics?song="
+
+def search(query):
+        r = requests.get(API + query)
+        find = r.json()
+        return find
+       
+def lyrics(query):
+        fin = search(query)
+        text = f'**🎶 Successfully Extracted Lyrics Of {song} 🎶**\n\n\n\n'
+        text += f'`{fin["lyrics"]}`'
+        return text
+
 st.set_page_config(page_title="Download Now",page_icon="kannan/logo.png",menu_items={
     "Get help": "https://github.com/dudegladiator/YoutubeDownloader",
     "Report a bug" : "https://github.com/dudegladiator/YoutubeDownloader/issues"
@@ -25,5 +39,7 @@ if(st.button('Submit')):
          thums = urlhp["album"]["cover_big"]
          thumb = wget.download(thums)
          st.image(thumb)
+         rpl = lyrics(query)
+         st.write(rpl)
      except Exception:
          st.write("Results Not Found")
