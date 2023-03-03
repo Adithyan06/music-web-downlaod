d032f26559ce2e911b027bd38935f3e077ee80b4
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-03 18:04:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if(st.button('Submit')):
              audios = ydl.prepare_filename(info_dict)
              ydl.process_info(info_dict)
              v = ydl.download(link)
-             st.video(v)
+             st.video(a)
 #            st.download_button("Save Audio",,file_name=f"{title}.mp4") 
      except Exception as e:
              st.write(e)
