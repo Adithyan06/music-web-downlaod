83bf5b35e212d3231eae635eb9f53c01bb098f72
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-04 08:05:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,53 +18,41 @@ st.set_page_config(page_title="Download Now or later",page_icon="images/logo.png
 st.title("Download Youtube Audio")
 query = st.text_input("Youtube Video or Playlist URL")
 option = st.radio("Select Type: ", ('Audio', 'Video'))
-
-if(st.button('Submit')):
-     ydl_opts = {
-             "format": "bv+ba/b",
-             "addmetadata": True,
-             "key": "FFmpegMetadata",
-             "prefer_ffmpeg": True,
-             "geo_bypass": True,
-             "nocheckcertificate": True,
-             "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
-             "outtmpl": "%(alt_title)s.mp4",
-             "quiet": True,
-             "logtostderr": False,
-     }
-     try:
-         results = []
-         count = 0
-         while len(results) == 0 and count < 6:
-             if count>0:
-                 time.sleep(1)
-             results = YoutubeSearch(query, max_results=1).to_dict()
-             count += 1
-         try:
-             link = f"https://youtube.com{results[0]['url_suffix']}"
-             title = results[0]["title"]
-             yt = YouTube(link)
-             if (option == 'Video'):
+try:
+      results = []
+      count = 0
+      while len(results) == 0 and count < 6:
+          if count>0:
+              time.sleep(0.5)
+          results = YoutubeSearch(query, max_results=1).to_dict()
+          count += 1
+      try:
+          link = f"https://youtube.com{results[0]['url_suffix']}"
+          title = results[0]["title"]
+           yt = YouTube(link)
+           if (option == 'Video'):
                  res = st.selectbox("Select The resolution",("1080p","720p","360p","240p","144p"))
-                 video = yt.streams.get_by_itag(yt.streams.filter(res=res , progressive="True" )[0].itag)      
-                 hi = video.download()           
-                 p = Path(hi)
-                 p=p.rename(p.with_name(f"{title}.mp4"))
-                 with open(p,'rb' ) as f:                
-                     st.write(f"{title}")
-#                    st.video(f)
-                     st.download_button("Save Video", data=f, file_name=f"{title}.mp4") 
-             else:
-                   audio = yt.streams.get_by_itag(yt.streams.filter(type="audio",mime_type="audio/webm")[0].itag)
-                   a = audio.download()
-                   ma = Path(a)
-                   ma=ma.rename(ma.with_name(f"{title}.mp3"))   
-                   with open(ma,'rb' ) as s:                
-                       st.write(f"{title}")
-#                      st.audio(s)
-                       st.download_button("Save Audio", data=s, file_name=f"{title}.mp3")      
-         except Exception as e:
-             st.info("Song not found")
-             st.write(e)
-     except Exception:
-         st.info("not found")
+                 if(st.button('Submit')):
+                      video = yt.streams.get_by_itag(yt.streams.filter(res=res , progressive="True" )[0].itag)      
+                      hi = video.download()           
+                      p = Path(hi)
+                      p=p.rename(p.with_name(f"{title}.mp4"))
+                      with open(p,'rb' ) as f:                
+                          st.write(f"{title}")
+#                         st.video(f)
+                          st.download_button("Save Video", data=f, file_name=f"{title}.mp4") 
+           else:
+                if(st.button('Submit')):
+                    audio = yt.streams.get_by_itag(yt.streams.filter(type="audio",mime_type="audio/webm")[0].itag)
+                    a = audio.download()
+                    ma = Path(a)
+                    ma=ma.rename(ma.with_name(f"{title}.mp3"))   
+                    with open(ma,'rb' ) as s:                
+                        st.write(f"{title}")
+#                       st.audio(s)
+                        st.download_button("Save Audio", data=s, file_name=f"{title}.mp3")      
+      except Exception as e:
+          st.info("Song not found")
+          st.write(e)
+except Exception:
+    st.info("not found")
