76848c8c7e2667db0fbce434edd76d2e5d85cd0d
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-04 09:20:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from pathlib import Path
 from pytube import YouTube
 import os , time,random,sys
 import time
-import wget
 import ffmpeg
-import yt_dlp
 from youtube_search import YoutubeSearch
+from streamlit_lottie import st_lottie
 
 st.cache()
-st.set_page_config(page_title="Download Now or later",page_icon="images/logo.png",menu_items={
+st.set_page_config(page_title="Download Any songs now !!",page_icon="images/logo.png",menu_items={
     "Get help": "https://github.com/dudegladiator/YoutubeDownloader",
-    "Report a bug" : "https://github.com/dudegladiator/YoutubeDownloader/issues"
+    "Report a bug" : ""
     
 })
 
+lottie_coding = load_lottieurl(https://assets6.lottiefiles.com/private_files/lf30_YcIv9u.json")
 st.title("Download Youtube Audio")
+image_column = st.columns((1))
+with image_column:
+        st_lottie(lottie_coding, height=300, key="music")
+st.caption("Download any Video/Audio Songs.Just copy the name or YouTube link of the song you want 🥳")
 query = st.text_input("Youtube Video or Playlist URL")
 option = st.radio("Select Type: ", ('Audio 🎶', 'Video 🎥'))
 try:
@@ -31,8 +35,9 @@ try:
           title = results[0]["title"]
           yt = YouTube(link)
           if (option == 'Video 🎥'):
-                res = st.selectbox("Select The resolution",("1080p","720p","360p","240p","144p"))
+                res = st.selectbox("Select The resolution",("720p","360p","240p","144p"))
                 if(st.button('Submit')):
+                     st.info("Please Wait....")
                      video = yt.streams.get_by_itag(yt.streams.filter(res=res , progressive="True" )[0].itag)      
                      hi = video.download()           
                      p = Path(hi)
@@ -43,13 +48,14 @@ try:
                          st.download_button("Save Video", data=f, file_name=f"{title}.mp4") 
           else:
                if(st.button('Submit')):
+                   st.info("Please Wait....")
                    audio = yt.streams.get_by_itag(yt.streams.filter(type="audio",mime_type="audio/webm")[0].itag)
                    a = audio.download()
                    ma = Path(a)
                    ma=ma.rename(ma.with_name(f"{title}.mp3"))   
                    with open(ma,'rb' ) as s:                
                        st.write(f"{title}")
-#                      st.audio(s)
+                       st.audio(s)
                        st.download_button("Save Audio", data=s, file_name=f"{title}.mp3")      
       except Exception as e:
           st.info("Song not found")
