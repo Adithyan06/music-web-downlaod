3846e9b32671fcea313d090dbf35651f4ca8c77e
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-06 10:38:16 +0530

    Update spotdl.py

--- handlers/spotdl.py
+++ handlers/spotdl.py
@@ -4,6 +4,7 @@ import subprocess
 from typing import List
 import streamlit as st
 import yt_dlp
+import spotdl
 
 def download_from_spotify(download_path: str, query: List[str]):
     os.mkdir(download_path)
@@ -24,5 +25,5 @@ def send_songs_from_directory(
         except Exception:
             st.write("Note Found")
     
-    subprocess.run(['ffmpeg','rm','-r', directory_path])  
+    subprocess.run(['spotdl -h','ffmpeg','rm','-r', directory_path])  
 
