7ee40cb60f3fbe3f52fbef1abf3c6a3a1e09823b
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-30 15:36:20 +0530

    Update spotdl.py

--- handlers/spotdl.py
+++ handlers/spotdl.py
@@ -25,15 +25,3 @@ def send_songs_from_directory(
             st.write("Note Found")
     
     subprocess.run(['ffmpeg','-r', directory_path])  
-
-@sync_to_async
-def getIds(video):
-    ids = []
-    with YoutubeDL({'quiet':True}) as ydl:
-        info_dict = ydl.extract_info(video, download=False)
-        try:
-            info_dict = info_dict['entries']
-            ids.extend([x.get('id'),x.get('playlist_index'),x.get('creator') or x.get('uploader'),x.get('title'),x.get('duration'),x.get('thumbnail')] for x in info_dict)
-        except:
-            ids.append([info_dict.get('id'),info_dict.get('playlist_index'),info_dict.get('creator') or info_dict.get('uploader'),info_dict.get('title'),info_dict.get('duration'),info_dict.get('thumbnail')])
-    return ids 
