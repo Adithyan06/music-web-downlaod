1eafd77275f9330df8fc0ba256e8b6f4b96147dc
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-30 15:54:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ try:
           time.sleep(0.3)
         results = YoutubeSearch(query, max_results=1).to_dict()
         count += 1
-    try:
+#   try:
         link = f"https://youtube.com{results[0]['url_suffix']}"
         title = results[0]["title"]
         yt = YouTube(link)
@@ -44,14 +44,17 @@ try:
                     st.write(f"{yt.title}")
                     st.video(f)
                     st.download_button("Save Video", data=f, file_name=f"{title[:35]}.mp4") 
-           else:                 
-               audio = yt.streams.get_by_itag(yt.streams.filter(type="audio",mime_type="audio/webm")[0].itag) 
-               a = audio.download()
-               ma = Path(a)
-               ma=ma.rename(ma.with_name(f"{title[:33]}.mp3"))  
-               if(st.button('Submit')): 
-                    st.spinner("Please Wait....")
-                    with open(ma,'rb' ) as s:                
-                        st.write(f"{title[:33]}")
-                        st.audio(s)
-                        st.download_button("Save Audio", data=s, file_name=f"{yt.title[:33]}.mp3")  
+        else:                 
+            audio = yt.streams.get_by_itag(yt.streams.filter(type="audio",mime_type="audio/webm")[0].itag) 
+            a = audio.download()
+            ma = Path(a)
+            ma=ma.rename(ma.with_name(f"{title[:33]}.mp3"))  
+            if(st.button('Submit')): 
+                 st.spinner("Please Wait....")
+                 with open(ma,'rb' ) as s:                
+                     st.write(f"{title[:33]}")
+                     st.audio(s)
+                     st.download_button("Save Audio", data=s, file_name=f"{yt.title[:33]}.mp3")  
+except Exception as e:
+    await st.error("𝖯𝗅𝖾𝖺𝗌𝖾 𝖳𝗋𝗒 𝖠𝗀𝖺𝗂𝗇 𝖮𝗋 𝖲𝖾𝖺𝗋𝖼𝗁 𝖺𝗍 Google.com 𝖥𝗈𝗋 𝖢𝗈𝗋𝗋𝖾𝖼𝗍 𝖲𝗉𝖾𝗅𝗅𝗂𝗇𝗀 𝗈𝖿 𝗍𝗁𝖾 Song")
+    print(str(e))
