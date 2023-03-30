75087a6a9bbd64a7d5f26bd49dc0e41d42e3521e
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-30 15:18:56 +0530

    Update spotdl.py

--- handlers/spotdl.py
+++ handlers/spotdl.py
@@ -3,7 +3,7 @@ import ffmpeg
 import subprocess
 from typing import List
 import streamlit as st
-import yt_dlp
+from yt_dlp import YoutubeDL
 
 def download_from_spotify(download_path: str, query: List[str]):
     os.mkdir(download_path)
@@ -26,3 +26,14 @@ def send_songs_from_directory(
     
     subprocess.run(['ffmpeg','-r', directory_path])  
 
+@sync_to_async
+def getIds(video):
+    ids = []
+    with YoutubeDL({'quiet':True}) as ydl:
+        info_dict = ydl.extract_info(video, download=False)
+        try:
+            info_dict = info_dict['entries']
+            ids.extend([x.get('id'),x.get('playlist_index'),x.get('creator') or x.get('uploader'),x.get('title'),x.get('duration'),x.get('thumbnail')] for x in info_dict)
+        except:
+            ids.append([info_dict.get('id'),info_dict.get('playlist_index'),info_dict.get('creator') or info_dict.get('uploader'),info_dict.get('title'),info_dict.get('duration'),info_dict.get('thumbnail')])
+    return ids 
