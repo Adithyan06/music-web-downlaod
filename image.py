58a016121961b089be07482942f40ca945e162c5
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-31 07:04:11 +0530

    Update image.py

--- image.py
+++ image.py
@@ -33,6 +33,6 @@ if(st.button('Submit')):
      link = f"http://api.safone.me/image?query={hello}&limit={color}"
      dato = requests.get(url=link).json()
      thums = dato.get("imageUrl")
-#    image = wget.download(thums)
-     st.image(dato)
+     image = wget.download(thums)
+     st.image(image)
 #    st.download_button("Download Image",data=image)
