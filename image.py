b17805b15653e4e6e58b57fcda07e87ab8db26f0
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-31 07:02:47 +0530

    Update image.py

--- image.py
+++ image.py
@@ -34,5 +34,5 @@ if(st.button('Submit')):
      dato = requests.get(url=link).json()
      thums = dato.get("imageUrl")
 #    image = wget.download(thums)
-     st.image(thums)
+     st.image(dato)
 #    st.download_button("Download Image",data=image)
