2f5b2953ca605ee262499e69f28409eeb6c9c46a
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-03-31 09:40:16 +0530

    Update image.py

--- image.py
+++ image.py
@@ -32,7 +32,7 @@ color = st.text_input("Enter Your Colour")
 if(st.button('Submit')):
      link = f"http://api.safone.me/image?query={hello}&limit={color}"
      dato = requests.get(url=link).json()
-     pr = dato['imageUrl']
+     pr = dato['results'][0]['imageUrl']
 #    image = wget.download(thums)
      st.write(pr)
 #    st.image(image)
