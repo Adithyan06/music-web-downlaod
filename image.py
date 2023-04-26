2e5507d637432f902eba2d93f25abc60a8af0647
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-04-26 09:48:27 +0530

    Update image.py

--- image.py
+++ image.py
@@ -12,29 +12,20 @@ st.set_page_config(page_title="Download Now",page_icon="🧡",menu_items={
 st.cache()
 st.title("Download any YouTube videos with best quality 🥳")
 URL = st.text_input("Paste any YouTube URL/Link")
-results = []
-count = 0
-while len(results) == 0 and count < 6:
-    if count>0:
-        time.sleep(1)
-    results = YoutubeSearch(URL, max_results=1).to_dict()
-    count += 1
-    try:
-       link = f"https://youtube.com{results[0]['url_suffix']}"
-       title = results[0]["title"]
-       yt = YouTube(link)
-       if(st.button('Apply')):
-            z = st.info("Please Wait...")
-#           ydl_opts = {"outtmpl": f"{yt.title}.mp4"}
-            with YoutubeDL() as ydl:
-                info = ydl.extract_info(link, download=False)
-                video = ydl.prepare_filename(info)
-                ydl.process_info(info)
-                p = Path(video)
-                p=p.rename(p.with_name(f"{title[:33]}.mp4"))
-                with open(p,'rb') as f:
-                    st.write(f"{title}")
-                    st.video(f)
-                    st.download_button("Download Video 📥",data=f,file_name=f"{title}.mp4")
-    except Exception:
-        st.error("❌')  
+results = YoutubeSearch(URL, max_results=1).to_dict()
+link = f"https://youtube.com{results[0]['url_suffix']}"
+title = results[0]["title"]
+yt = YouTube(link)
+if(st.button('Apply')):
+     z = st.info("Please Wait...")
+#    ydl_opts = {"outtmpl": f"{yt.title}.mp4"}
+      with YoutubeDL() as ydl:
+          info = ydl.extract_info(link, download=False)
+          video = ydl.prepare_filename(info)
+          ydl.process_info(info)
+          p = Path(video)
+          p=p.rename(p.with_name(f"{title[:33]}.mp4"))
+          with open(p,'rb') as f:
+              st.write(f"{title}")
+              st.video(f)
+              st.download_button("Download Video 📥",data=f,file_name=f"{title}.mp4")
