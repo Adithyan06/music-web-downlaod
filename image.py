5d59984e5005ea5fcaee661546c83e2bf080f700
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-04-26 07:01:31 +0530

    Update image.py

--- image.py
+++ image.py
@@ -2,16 +2,6 @@ import streamlit as st
 from pathlib import Path
 from yt_dlp import YoutubeDL
 from pytube import YouTube
-from streamlit_lottie import st_lottie
-import requests
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-lottie_coding = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_bwk2zS.json")
 
 st.set_page_config(page_title="Download Now",page_icon="🧡",menu_items={
     "Get help": "https://github.com/dudegladiator/YoutubeDownloader",
@@ -19,14 +9,12 @@ st.set_page_config(page_title="Download Now",page_icon="🧡",menu_items={
     
 })                       
 st.cache()
-st_lottie(lottie_coding, height=300, key="coding")
-
 st.title("Download any YouTube videos with best quality 🥳")
 URL = st.text_input("Paste any YouTube URL/Link")
 yt = YouTube(URL)
 if(st.button('Apply')):
      z = st.info("Please Wait...")
-     ydl_opts = {"outtmpl": f"{yt.title}.mp4"}
+#    ydl_opts = {"outtmpl": f"{yt.title}.mp4"}
      with YoutubeDL() as ydl:
          info = ydl.extract_info(URL, download=False)
          video = ydl.prepare_filename(info)
