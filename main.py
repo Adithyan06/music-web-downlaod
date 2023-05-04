35e2711d39ef8418277653f1df45bd860a7cb7ca
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-04 06:21:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,10 @@ if(st.button('Submit')):
                   data={'text':query},
                   headers={'api-key':'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
                 )
-                st.image(r.json())
+                i = path(r.json())
+                i=i.rename(i.with_name("Image.jpg")
+                with open(i,'rb') as b:
+                    st.image(b)
              else:                
                  with YoutubeDL() as ydl:
                      info = ydl.extract_info(query, download=False)
