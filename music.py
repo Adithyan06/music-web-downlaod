9964d770ff3af1719b8a5a59eaa46620b4acd71f
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-09 18:45:09 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,20 +1,7 @@
 import streamlit as st
-
 from PIL import Image, ImageDraw, ImageFont
 
-st.cache()
-
-st.set_page_config(
-
-    page_title="Download Any songs now !!",
-
-    page_icon="random",
-
-    menu_items={"Get help": "https://github.com/Adithyan06"})
-
-st.title("YouTube Videos Downloader") 
-
-def generate_logo(text, font_size, text_color, bg_color, output_path):
+ def generate_logo(text, font_size, text_color, bg_color, output_path):
 
     # Create a blank image with the desired background color
 
@@ -45,31 +32,23 @@ def generate_logo(text, font_size, text_color, bg_color, output_path):
     image.save(output_path)
 
 # Main function to run the Streamlit app
-
-def main():
-
-    st.title("Logo Generator")
+st.cache()
+st.set_page_config(
+   page_title="Download Any songs now !!",
+   page_icon="random",
+   menu_items={"Get help": "https://github.com/Adithyan06"}
+    
+st.title("Logo Generator")
 
     # User inputs
+text = st.text_input("Enter text for the logo:")
+font_size = st.slider("Select font size:", 20, 100, 50)
+text_color = st.color_picker("Select text color:", "#FFFFFF")
+bg_color = st.color_picker("Select background color:", "#000000")
+output_path = st.text_input("Enter output path to save the logo:", "logo.png")# Generate and display the logo
 
-    text = st.text_input("Enter text for the logo:")
-
-    font_size = st.slider("Select font size:", 20, 100, 50)
-
-    text_color = st.color_picker("Select text color:", "#FFFFFF")
-
-    bg_color = st.color_picker("Select background color:", "#000000")
-
-    output_path = st.text_input("Enter output path to save the logo:", "logo.png")
-
-    # Generate and display the logo
-
-    if st.button("Generate Logo"):
-
-        generate_logo(text, font_size, text_color, bg_color, output_path)
-
-        st.success("Logo generated successfully!")
-
-        # Display the generated logo
-
-        st.image(output_path)
+if st.button("Generate Logo"):
+    generate_logo(text, font_size, text_color, bg_color, output_path)
+    st.success("Logo generated successfully!")
+    # Display the generated logo
+    st.image(output_path)
