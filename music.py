23c84f380f7aa8f9cba987d10041f787d2064f5a
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-09 18:34:37 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,32 +1,63 @@
-import os , time,random,sys
 import streamlit as st
-from pathlib import Path
-import os
-import ffmpeg
-import subprocess
-from typing import List
-import uuid
-import traceback
 
-from handlers import spotdl
+from PIL import Image, ImageDraw, ImageFont
 
-st.cache()
+def generate_logo(text, font_size, text_color, bg_color, output_path):
 
-# From here Website start
-st.set_page_config(page_title="Download Songs Now",page_icon="images/logo.png",menu_items={
-    "Get help": "https://github.com/dudegladiator/YoutubeDownloader",
-    "Report a bug" : "https://github.com/dudegladiator/YoutubeDownloader/issues"
-    
-})
-st.title("Download song from spotify") 
-query = st.text_input("Spotify url")
-if(st.button('Submit')):
-     result = query.title()
-     st.info(result)     
-     download_path = os.getcwd() + "/" + str(uuid.uuid4())
-     try:
-         spotdl.download_from_spotify(download_path, query)
-         spotdl.send_songs_from_directory(download_path, query)
-     except Exception as e:
-         print(e)
-         st.write(e)
+    # Create a blank image with the desired background color
+
+    image = Image.new('RGB', (500, 500), bg_color)
+
+    draw = ImageDraw.Draw(image)
+
+    # Load a font (adjust the path to your font file)
+
+    font = ImageFont.truetype("Arial.ttf", font_size)
+
+    # Calculate the width and height of the text
+
+    text_width, text_height = draw.textsize(text, font=font)
+
+    # Calculate the x and y coordinates to center the text
+
+    x = (image.width - text_width) // 2
+
+    y = (image.height - text_height) // 2
+
+    # Draw the text on the image
+
+    draw.text((x, y), text, font=font, fill=text_color)
+
+    # Save the generated logo to the output path
+
+    image.save(output_path)
+
+# Main function to run the Streamlit app
+
+def main():
+
+    st.title("Logo Generator")
+
+    # User inputs
+
+    text = st.text_input("Enter text for the logo:")
+
+    font_size = st.slider("Select font size:", 20, 100, 50)
+
+    text_color = st.color_picker("Select text color:", "#FFFFFF")
+
+    bg_color = st.color_picker("Select background color:", "#000000")
+
+    output_path = st.text_input("Enter output path to save the logo:", "logo.png")
+
+    # Generate and display the logo
+
+    if st.button("Generate Logo"):
+
+        generate_logo(text, font_size, text_color, bg_color, output_path)
+
+        st.success("Logo generated successfully!")
+
+        # Display the generated logo
+
+        st.image(output_path)
