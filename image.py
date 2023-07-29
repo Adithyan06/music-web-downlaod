2b1dd85f4427403c7aaf053c1b743393bf40995a
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-07-29 21:03:20 +0530

    Update image.py

--- image.py
+++ image.py
@@ -30,9 +30,9 @@ if(st.button('Apply')):
                 p = Path(video)
 #                p=p.rename(p.with_name(f"{title[:33]}.mp4"))
 #                with open(p,'rb') as f:
-                    st.write(f"{title}")
-                    st.video(video)
-                    st.download_button("Download Video 📥",data=video,file_name=f"{title}.mp4")
+                st.write(f"{title}")
+                st.video(video)
+                st.download_button("Download Video 📥",data=video,file_name=f"{title}.mp4")
          else:
               with YoutubeDL() as ydl:
                   info = ydl.extract_info(URL, download=False)
