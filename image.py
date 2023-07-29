17850b06e7b83069db602f731ff6e31d0cf84461
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-07-29 21:18:23 +0530

    Update image.py

--- image.py
+++ image.py
@@ -18,7 +18,7 @@ option = st.radio("Select Type: ", ('YouTube', 'URL Upload'))
 if(st.button('Apply')):
      with st.spinner('Downloading...'):
          if (option == 'YouTube'):
-            ydl_opts = {"outtmpl": "%(title)s.mp4"}
+            ydl_opts = {"outtmpl": "%(id)s.mp4"}
             results = YoutubeSearch(URL, max_results=1).to_dict()
             link=f"https://youtube.com{results[0]['url_suffix']}"
             title = results[0]["title"]
