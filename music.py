51353ba35166113c6643abda8fae8f729dc4579d
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-07-30 17:49:42 +0530

    Update music.py

--- music.py
+++ music.py
@@ -43,7 +43,7 @@ def main():
                    wallpaper = wall['results'][0]['imageUrl']
 #                   file = wget.download(wallpaper)
                    st.image(wallpaper)  
-                   st.download_button("Save Image", data=wallpaper, file_name=f"({wall['results'][0]['title']}).jpg")
+                   st.download_button("Save Image", data=wallpaper, file_name=f"{query}.jpg")
             
 if __name__ == "__main__":
     main()
