6d5832a996071bbc433b0b74abdddc151f1e8170
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-07-30 16:31:42 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,48 +1,28 @@
 import streamlit as st
-
 import requests
 
 def search_wallpapers(query, max_results=10):
-
     url = f"https://api.unsplash.com/search/photos"
-
     params = {
-
         "query": query,
-
         "client_id": "dKRzg3P20iERjrsD0_rIOhSYpVAYLTWtlYXhKDA5T-Y",
-
         "per_page": max_results,
-
         "orientation": "landscape",  # Forces landscape-oriented wallpapers
-
         "content_filter": "high",  # Filters for high-quality wallpapers
-
     }
-
     response = requests.get(url, params=params)
-
     data = response.json()
-
     return data["results"]
 
 def display_wallpapers(wallpapers, max_results=10):
-
     count = 0
-
     for wallpaper in wallpapers:
-
         if count >= max_results:
-
             break
-
         st.image(wallpaper["urls"]["regular"], caption=wallpaper["alt_description"])
-
 #       st.write(f"Download: [Link]({wallpaper['links']['download']})")
         st.download_button("Save Image", data=f"({wallpaper['urls']['regular']})", file_name="image.jpg") 
-
         count += 1
-
 
 def main():
 
@@ -58,7 +38,8 @@ def main():
     else:
         url = f"http://api.safone.me/wall?query={query}&limit=5"
         wall = requests.get(url=url).json()
-    
+        wallpaper = wall.get('imageUrl')
+        st.write(wallpaper)
+        
 if __name__ == "__main__":
-
     main()
