f662d3c2fc8d9057e0ca3785bf50bae878601179
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-07-30 16:59:02 +0530

    Update music.py

--- music.py
+++ music.py
@@ -38,9 +38,9 @@ def main():
     else:
         url = f"http://api.safone.me/wall?query={query}&limit=5"
         wall = requests.get(url=url).json()
-        wallpaper = wall.get('results')
-        wallpapers = wallpaper['imageUrl']
-        st.write(wall)
+        wallpaper = wall['results']['imageUrl']
+#        wallpapers = wallpaper['imageUrl']
+        st.write(wallpaper)
         
 if __name__ == "__main__":
     main()
